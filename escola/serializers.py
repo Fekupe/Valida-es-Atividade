349b47bd761c5de94437cf695459c27e8d636b98
Gabriel, Candido <fekupe@outlook.com>
2024-10-17 14:38:23 -0300

    Validações

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,10 +1,58 @@
+import datetime
 from rest_framework import serializers
 from escola.models import Estudante,Curso, Matricula
+from datetime import date
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
         fields = ['id','nome','email','cpf','data_nascimento','celular']
+
+    def validate_cpf(self, cpf: str) -> str: 
+        if len(cpf) !=11:
+            raise serializers.ValidationError('O CPF deve ter 11 dígitos')
+        
+        #Extrair os dígitos
+        digits = [int(d) for d in cpf]
+
+        #Calcular primeiro dígito de validação
+        weights = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+        soma1 = sum(d * w for d , w in zip(digits[:9], weights))
+        resto1 = soma1 % 11
+        first_check_digit = 0 if resto1 in [0,1] else 11 - resto1
+
+        #Calcular o segundo dígito de validação
+        weights2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+        soma2 = sum(d * w for d, w in zip(digits[:10], weights2))
+        resto2 = soma2 % 11
+        second_check_digit = 0 if resto2 in [0,1] else 11 - resto2
+
+        #Validação
+        if digits [-2] == first_check_digit and digits [-1] == second_check_digit:
+            return cpf
+        else:
+            raise serializers.ValidationError('CPF Inválido')
+        
+        
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('O nome deve conter apenas letras')
+        return nome
+    
+    def validate_celular(self, celular):
+        if len(celular) !=11:
+            raise serializers.ValidationError('O Celular deve ter 11 dígitos com DDD')
+        return celular 
+
+    def validate_email(self, email):
+        if '@' not in email:
+            raise serializers.ValidationError('Insira um Email Válido')
+        return email
+    
+    def validate_data_nascimento(self, data_nascimento):
+        if data_nascimento > datetime.date.today():
+            raise serializers.ValidationError('Informe uma data de nascimento válida')
+        return data_nascimento 
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,6 +63,13 @@ class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         exclude = []
+
+    # def validate_curso (self, data):
+    #     estudante = data['estudante']
+    #     curso = data['curso']
+    #     periodo = data['periodo']
+
+    #     if self.vali
 
 class ListaMatriculasEstudanteSerializer(serializers.ModelSerializer):
     curso = serializers.ReadOnlyField(source='curso.descricao')
